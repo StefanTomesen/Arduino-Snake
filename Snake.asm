39792ebfb4bba99e858d4c44783caa07d7d15af5
Thism2 <thism.rasse@hotmail.com>
2014-04-28 12:11:51 +0200

    Första test

--- Snake.asm
+++ Snake.asm
@@ -3,7 +3,8 @@
  */ 
 	
 	// Register definitions
-	.DEF	rTemp = r16
+	.DEF	rTemp		= r16
+	.DEF	rCounter	= r17
 
 	// Constant definitions
 	.EQU	NUM_COLUMNS = 8
@@ -12,7 +13,7 @@
 	// Data Segment
 	.DSEG
 	matrix:	.BYTE 8			// LED matrix - 1 bit per "pixel"
-
+	jumptable: .BYTE 16
 	// Code segment
 	.CSEG
 	.ORG 0x0000
@@ -29,6 +30,7 @@ init:
 	out SPH, rTemp
 	ldi rTemp, LOW(RAMEND)
 	out SPL, rTemp
+
 
 	// Set row bits as output bits
 	sbi DDRC, 0	
@@ -78,22 +80,83 @@ init:
 	
 
 render:
+	// Delay
+
+	// clear all input
+	cbi PORTC, 0		// row 0
+	cbi PORTC, 1		// row 1
+	cbi PORTC, 2		// row 2
+	cbi PORTC, 3		// row 3
+	cbi PORTD, 2		// row 4
+	cbi PORTD, 3		// row 5
+	cbi PORTD, 4		// row 6
+	cbi PORTD, 5		// row 7
+
+	cbi PORTD, 6		// column 0
+	cbi PORTD, 7		// column 1
+	cbi PORTB, 0		// column 2
+	cbi PORTB, 1		// column 3
+	cbi PORTB, 2		// column 4
+	cbi PORTB, 3		// column 5
+	cbi PORTB, 4		// column 6
+	cbi PORTB, 5		// column 7
+
+	subi rCounter, -1
+
+	cpi rCounter, 1
+	breq case1
+
+	cpi rCounter, 2
+	breq case2
+	
+	cpi rCounter, 3
+	breq case3
+	
+	cpi rCounter, 4
+	breq case4
+	
+	cpi rCounter, 5
+	breq case5
+	
+	cpi rCounter, 6
+	breq case6
+	
+	cpi rCounter, 7
+	breq case7
+	
+	cpi rCounter, 8
+	breq case8
+
+case1:
 	sbi PORTC, 0		// row 0
-	sbi PORTC, 1		// row 1
-	sbi PORTC, 2		// row 2
-	sbi PORTC, 3		// row 3
-	sbi PORTD, 2		// row 4
-	sbi PORTD, 3		// row 5
-	sbi PORTD, 4		// row 6
-	sbi PORTD, 5		// row 7
-
 	sbi PORTD, 6		// column 0
-	sbi PORTD, 7		// column 1
-	sbi PORTB, 0		// column 2
-	sbi PORTB, 1		// column 3
+	jmp render
+case2:
+	sbi PORTC, 1		// row 0
+	sbi PORTD, 7		// column 0
+	jmp render
+case3:
+	sbi PORTC, 2		// row 0
+	sbi PORTB, 0		// column 0
+	jmp render
+case4:
+	sbi PORTC, 3		// row 4
+	sbi PORTB, 1		// column 4
+	jmp render
+case5:
+	sbi PORTD, 2		// row 4
 	sbi PORTB, 2		// column 4
-	sbi PORTB, 3		// column 5
-	sbi PORTB, 4		// column 6
-	sbi PORTB, 5		// column 7
-
+	jmp render
+case6:
+	sbi PORTD, 3		// row 4
+	sbi PORTB, 3		// column 4
+	jmp render
+case7:
+	sbi PORTD, 4		// row 4
+	sbi PORTB, 4		// column 4
+	jmp render
+case8:
+	sbi PORTD, 5		// row 4
+	sbi PORTB, 5		// column 4
+	ldi rCounter, 0
 	jmp render
