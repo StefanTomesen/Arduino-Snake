c31646808a38a9cf6e22962f13cac713aad22b41
StefanTomesen <stefantomesen@gmail.com>
2014-04-28 11:18:37 +0200

    All rows

--- Snake.asm
+++ Snake.asm
@@ -30,10 +30,70 @@ init:
 	ldi rTemp, LOW(RAMEND)
 	out SPL, rTemp
 
-	sbi DDRD, 6
-	sbi DDRC, 0
+	// Set row bits as output bits
+	sbi DDRC, 0	
+	sbi DDRC, 1	
+	sbi DDRC, 2	
+	sbi DDRC, 3	
+	sbi DDRD, 2	
+	sbi DDRD, 3	
+	sbi DDRD, 4	
+	sbi DDRD, 5	
+
+	// Set column bits as output bits
+	sbi DDRD, 6	
+	sbi DDRD, 7	
+	sbi DDRB, 0	
+	sbi DDRB, 1	
+	sbi DDRB, 2	
+	sbi DDRB, 3	
+	sbi DDRB, 4	
+	sbi DDRB, 5
+
+
+/*
+	// set bit row table
+
+	sbi PORTC, 0		// row 0
+	sbi PORTC, 1		// row 1
+	sbi PORTC, 2		// row 2
+	sbi PORTC, 3		// row 3
+	sbi PORTD, 2		// row 4
+	sbi PORTD, 3		// row 5
+	sbi PORTD, 4		// row 6
+	sbi PORTD, 5		// row 7
+
+	// set bit column table
+
+	sbi PORTD, 6		// column 0
+	sbi PORTD, 7		// column 1
+	sbi PORTB, 0		// column 2
+	sbi PORTB, 1		// column 3
+	sbi PORTB, 2		// column 4
+	sbi PORTB, 3		// column 5
+	sbi PORTB, 4		// column 6
+	sbi PORTB, 5		// column 7
+*/
+
+	
 
 render:
-	sbi PORTD, 6
-	sbi PORTC, 0
+	sbi PORTC, 0		// row 0
+	sbi PORTC, 1		// row 1
+	sbi PORTC, 2		// row 2
+	sbi PORTC, 3		// row 3
+	sbi PORTD, 2		// row 4
+	sbi PORTD, 3		// row 5
+	sbi PORTD, 4		// row 6
+	sbi PORTD, 5		// row 7
+
+	sbi PORTD, 6		// column 0
+	sbi PORTD, 7		// column 1
+	sbi PORTB, 0		// column 2
+	sbi PORTB, 1		// column 3
+	sbi PORTB, 2		// column 4
+	sbi PORTB, 3		// column 5
+	sbi PORTB, 4		// column 6
+	sbi PORTB, 5		// column 7
+
 	jmp render
